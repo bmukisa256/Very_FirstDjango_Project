f4b4fc0e41dcb75e3c05a04e0047c533373aaf6e
bmukisa256 <bmukisa13@gmail.com>
2024-10-22 17:33:16 +0300

    Update on project.

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),  # Root URL mapped to HomeView
     path('home', views.HomeView.as_view()),
     path('authorization', views.AuthorizationView.as_view())
 ]
